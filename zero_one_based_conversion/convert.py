05c516b0a1e06c2a4ada9aed65c5a1de17c41b50
Ben Ainscough <bainscou@genome.wustl.edu>
2017-01-19 11:20:21 -0700

    Added travis ci integration testing

--- zero_one_based_conversion/convert.py
+++ zero_one_based_conversion/convert.py
@@ -8,7 +8,7 @@ def coordinate_system(input_string, convert_to_coordinate_system):
     Args:
         input_string (str): A tab-delimited string of one or many coordinates
             (separated by newline characters). Valid coordinates contain
-            chromosome, start position, stop position, reference base, and
+            chromosomee, start position, stop position, reference base, and
             variant base, in that order. Additional columns are accepted and
             appended on the converted string.
         convert_to_coordinate_system ('to_one_based', 'to_zero_based'): The
@@ -47,7 +47,7 @@ def coordinate_system(input_string, convert_to_coordinate_system):
                 raise TypeError('\'{0}\' is not a valid argument. Please use '
                                 '\'to_zero_based\' or '
                                 '\'to_one_based\'.'.format(
-                    convert_to_coordinate_system))
+                                    convert_to_coordinate_system))
         except ValueError as error:
             print('Coordinate Error in line {0} or input. Coordinate skipped.'
                   '\n\tMessage: {1}'.format(line_number, error))
