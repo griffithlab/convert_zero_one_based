d0073e7b1f3c040f84aedfb711f2e2d6fd45527c
Alex H. Wagner, PhD <awagner24@wustl.edu>
2018-04-02 10:50:49 -0500

    add dist

---
+++ build/lib/zero_one_based_conversion/convert.py
@@ -0,0 +1,56 @@
+import re
+from zero_one_based_conversion.coordinate import Coordinate
+
+
+def coordinate_system(input_string, convert_to_coordinate_system):
+    """Converts strings of cordinates to one-based or zero-based
+
+    Args:
+        input_string (str): A tab-delimited string of one or many coordinates
+            (separated by newline characters). Valid coordinates contain
+            chromosomee, start position, stop position, reference base, and
+            variant base, in that order. Additional columns are accepted and
+            appended on the converted string.
+        convert_to_coordinate_system ('to_one_based', 'to_zero_based'): The
+            coordinate system you would like to convert to.
+
+    Returns:
+        str: Tab-delimited string of the converted coordinates, multiple
+            coordinates are separated by newline characters.
+    """
+    output_string = ''
+    line_number = 0
+    for line in input_string.split('\n'):
+        try:
+            line_number += 1
+            pattern = re.compile("\w+\t\d+\t\d+\t[ACGT\-\.0]+\t[ACGT\-\.0]+.*")
+            if line == '':
+                continue  # skip empty lines
+            elif not pattern.match(line):
+                raise ValueError('Invalid format, expected chr, start, stop, '
+                                 'ref, var. Line: {0}'.format(line))
+            coord = line.split('\t')
+            extra_cols = []
+            if len(coord) > 5:
+                extra_cols = coord[5:]
+                del coord[5:]
+
+            if convert_to_coordinate_system is 'to_zero_based':
+                out = '\t'.join([Coordinate(coord[0], coord[1], coord[2],
+                                            coord[3], coord[4])
+                                .to_zero_based()] + extra_cols)
+            elif convert_to_coordinate_system is 'to_one_based':
+                out = '\t'.join([Coordinate(coord[0], coord[1], coord[2],
+                                            coord[3], coord[4])
+                                .to_one_based()] + extra_cols)
+            else:
+                raise TypeError('\'{0}\' is not a valid argument. Please use '
+                                '\'to_zero_based\' or '
+                                '\'to_one_based\'.'.format(
+                                    convert_to_coordinate_system))
+        except ValueError as error:
+            print('Coordinate Error in line {0} or input. Coordinate skipped.'
+                  '\n\tMessage: {1}'.format(line_number, error))
+            continue
+        output_string += out + '\n'
+    return output_string
---
+++ build/lib/zero_one_based_conversion/tests/test_convert.py
@@ -0,0 +1,84 @@
+from unittest import TestCase
+from zero_one_based_conversion import convert
+
+
+class TestConvert(TestCase):
+    def test_convert(self):
+        one_based = '''1	880975	880975	T	G
+1	2443098	2443098	A	T
+1	11187133	11187133	C	T
+7	14053448	14053450	AAG	CAT
+7	14053447	14053448	-	TTA
+7	14053448	14053448	A	-
+'''
+        one_based_extra_columns = '''1	880975	880975	T	G	S
+1	2443098	2443098	A	T	S
+1	11187133	11187133	C	T	S
+7	14053448	14053450	AAG	CAT	F
+7	14053447	14053448	-	TTA	F
+7	14053448	14053448	A	-	F
+'''
+        zero_based = '''1	880974	880975	T	G
+1	2443097	2443098	A	T
+1	11187132	11187133	C	T
+7	14053447	14053450	AAG	CAT
+7	14053447	14053447	-	TTA
+7	14053447	14053448	A	-
+'''
+        zero_based_extra_columns = '''1	880974	880975	T	G	S
+1	2443097	2443098	A	T	S
+1	11187132	11187133	C	T	S
+7	14053447	14053450	AAG	CAT	F
+7	14053447	14053447	-	TTA	F
+7	14053447	14053448	A	-	F
+'''
+        mixed_coords = '''1	880975	880975	T	G
+1	2443098	2443098	A	T
+1	11187133	11187133	C	T
+7	14053448	14053450	AAG	CAT
+7	14053447	14053448	-	TTA
+7	14053448	14053448	A	-
+1	880974	880975	T	G
+1	2443097	2443098	A	T
+1	11187132	11187133	C	T
+7	14053447	14053450	AAG	CAT
+7	14053447	14053447	-	TTA
+7	14053447	14053448	A	-
+'''
+        one_based_empty_lines = '''1	880975	880975	T	G
+1	2443098	2443098	A	T
+1	11187133	11187133	C	T
+7	14053448	14053450	AAG	CAT
+
+7	14053447	14053448	-	TTA
+7	14053448	14053448	A	-
+'''
+        misformated_coords_string = '''1	880975	880975	T	G
+1	2443098	2443098	A	T
+1	11187133	11187133	5	T
+7	A	14053450	AAG	CAT
+-	14053447	14053448	-	TTA
+7	14053448	14053448	A	-
+'''
+        misformated_coords_zero = '''1	880974	880975	T	G
+1	2443097	2443098	A	T
+7	14053447	14053448	A	-
+'''
+        one_to_zero = convert.coordinate_system(one_based, 'to_zero_based')
+        self.assertMultiLineEqual(one_to_zero, zero_based)
+        one_to_zero_extra_cols = convert.coordinate_system(
+            one_based_extra_columns, 'to_zero_based')
+        self.assertMultiLineEqual(one_to_zero_extra_cols,
+                                  zero_based_extra_columns)
+        zero_to_one = convert.coordinate_system(zero_based, 'to_one_based')
+        self.assertMultiLineEqual(zero_to_one, one_based)
+        mixed_coords_convert = convert.coordinate_system(mixed_coords,
+                                                         'to_zero_based')
+        self.assertMultiLineEqual(mixed_coords_convert,
+                                  zero_based+zero_based)
+        one_based_empty_lines_to_zero = convert.coordinate_system(
+            one_based_empty_lines, 'to_zero_based')
+        self.assertMultiLineEqual(one_based_empty_lines_to_zero, zero_based)
+        misformated_coords = convert\
+            .coordinate_system(misformated_coords_string, 'to_zero_based')
+        self.assertMultiLineEqual(misformated_coords, misformated_coords_zero)
